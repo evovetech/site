d428ff4cd94d7eb92e5a1dbf056af7c4bb61c612
Layne Penney <layne.penney@aa.com>
2017-10-30 07:33:05 -0500

    rebuilding site Mon Oct 30 07:33:05 CDT 2017

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -am "$msg"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
@@ -11,10 +16,6 @@ cd public
 git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
 git commit -m "$msg"
 
 # Push source and build repos.
